fbfadd36c919f5892fa180d72a4a29b0a5fbe0ce
yohira0616 <yukito.sys.so@gmail.com>
2017-04-13 22:09:10 +0900

    implement tasks

--- lib/diffcop.rb
+++ lib/diffcop.rb
@@ -2,6 +2,9 @@ require "diffcop/version"
 
 module Diffcop
   def self.execute
-    # TODO: imolements
+    diff_files = `git diff --name-only HEAD master`
+    targets = diff_files.split('\n').grep(%r{^app/.*(\.rb)$})
+    target_str = targets.join(' ')
+    puts `bundle exec rubocop -a #{target_str}` if target_str.present?
   end
 end
