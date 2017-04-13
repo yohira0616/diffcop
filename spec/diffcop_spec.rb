d08181c8df0d875561d2e927b4a0a32338d69dcd
yohira0616 <yukito.sys.so@gmail.com>
2017-04-14 01:12:55 +0900

    fix separator

--- spec/diffcop_spec.rb
+++ spec/diffcop_spec.rb
@@ -11,19 +11,19 @@ RSpec.describe Diffcop do
 
   describe '#rb_files' do
     it 'got some_controller.rb and awesome.rb' do
-      diff_files = 'app/controllers/some_controller.rb\napp/models/great_model/awesome.rb\napp/views/some/index.erb\n/javasciprt/src/index.js'
+      diff_files = "app/controllers/some_controller.rb\napp/models/great_model/awesome.rb\napp/views/some/index.erb\n/javasciprt/src/index.js"
       result = @executor.send(:rb_files,diff_files)
       expect(result).to eq 'app/controllers/some_controller.rb app/models/great_model/awesome.rb'
     end
 
     it 'got empty' do
-      diff_files = 'app/views/some/index.erb\njavasciprt/src/index.js'
+      diff_files = "app/views/some/index.erb\njavasciprt/src/index.js"
       result = @executor.send(:rb_files,diff_files)
       expect(result).to eq ''
     end
 
     it 'got some_controller.rb' do
-      diff_files = 'app/controllers/some_controller.rb\napp/views/some/index.erb\n/javasciprt/src/index.js'
+      diff_files = "app/controllers/some_controller.rb\napp/views/some/index.erb\n/javasciprt/src/index.js"
       result = @executor.send(:rb_files,diff_files)
       expect(result).to eq 'app/controllers/some_controller.rb'
     end
